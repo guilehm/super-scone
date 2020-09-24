dd1f4853edf2480483a9da14526928728cea4ced
guilherme <guile.hm@hotmail.com>
2020-09-24 20:25:29 -0300

    create simple crop url

--- scone/crop/urls.py
+++ scone/crop/urls.py
@@ -5,5 +5,10 @@ from scone.crop import views
 app_name = 'crop'
 
 urlpatterns = [
-    url(r'crop/(?P<width>\d{1,4})x(?P<height>\d{1,4})/(?P<url>(.*))', views.simple_crop, name='simple-crop'),
+    url(
+        r'crop/(?P<width>\d{1,4})x(?P<height>\d{1,4})/'
+        r'(?P<url>http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)',
+        views.simple_crop,
+        name='simple-crop'
+    ),
 ]
