c452a3518a65cb18b6d1982ecfb082d16e0ab007
guilherme <guile.hm@hotmail.com>
2020-09-25 20:59:20 -0300

    improve url

--- scone/crop/urls.py
+++ scone/crop/urls.py
@@ -4,11 +4,14 @@ from scone.crop import views
 
 app_name = 'crop'
 
+
 urlpatterns = [
     url(
-        r'crop/(?P<width>\d{1,4})x(?P<height>\d{1,4})/'
+        r'crop/'
+        r'(?P<fit>smartcrop|cover|contain|fill|inside|outside)/'
+        r'(?P<width>\d{1,4})x(?P<height>\d{1,4})/'
         r'(?P<url>http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)',
-        views.simple_crop,
-        name='simple-crop'
+        views.get_crop,
+        name='get-crop'
     ),
 ]
